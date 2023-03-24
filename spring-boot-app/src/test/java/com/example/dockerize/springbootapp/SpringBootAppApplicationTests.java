ad34303a25fc0746e55a3d98c2436aae4e372a2f
Tajiakil1 <tajimuhammadny@gmail.com>
2023-03-24 12:57:34 -0400

    a

--- spring-boot-app/src/test/java/com/example/dockerize/springbootapp/SpringBootAppApplicationTests.java
+++ spring-boot-app/src/test/java/com/example/dockerize/springbootapp/SpringBootAppApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.dockerize.springbootapp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootTest
-class SpringBootAppApplicationTests {
+@SpringBootApplication
+@RestController
+@RequestMapping("/api/hello")
+public class SpringBootAppApplication {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppApplication.class, args);
 	}
 
+	@GetMapping()
+	public String getGreeting(){
+		return "Hello spring boot application";
+	}
 }
